328e4acc52b51d5fe0398ce25667db375ad8ad8b
Martin <martin.preusse@gmail.com>
2021-03-21 13:50:46 +0100

    indexes script before constraints script

--- load_to_omop/entrypoint.sh
+++ load_to_omop/entrypoint.sh
@@ -2,7 +2,8 @@ while !</dev/tcp/postgres/5432; do sleep 1; done;
 
 Rscript /load_synthea.r
 
+PGPASSWORD=$POSTGRES_PASSWORD PGOPTIONS="--search_path=cdm_synthea10" psql -U $POSTGRES_USER -h postgres synthea -f /output/postgresql_5.3.1_index_ddl.sql
+
 PGPASSWORD=$POSTGRES_PASSWORD PGOPTIONS="--search_path=cdm_synthea10" psql -U $POSTGRES_USER -h postgres synthea -f /output/postgresql_5.3.1_constraint_ddl.sql
 
-PGPASSWORD=$POSTGRES_PASSWORD PGOPTIONS="--search_path=cdm_synthea10" psql -U $POSTGRES_USER -h postgres synthea -f /output/postgresql_5.3.1_index_ddl.sql
 
