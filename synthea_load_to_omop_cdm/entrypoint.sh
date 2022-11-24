3b3af6eec6d764b22705978133e21dc3cfdf7f04
Martin Preusse <martin.preusse@gmail.com>
2022-11-24 12:10:56 +0100

    simplify synthea repo

--- synthea_load_to_omop_cdm/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#while !</dev/tcp/postgres/5432; do sleep 1; done;
-
-Rscript /load_synthea.r
-#
-#PGPASSWORD=$POSTGRES_PASSWORD PGOPTIONS="--search_path=cdm_synthea10" psql -p 15432 -U $POSTGRES_USER -h $POSTGRES_HOST $POSTGRES_DB -f /output/postgresql_5.3.1_index_ddl.sql
-#
-#PGPASSWORD=$POSTGRES_PASSWORD PGOPTIONS="--search_path=cdm_synthea10" psql -p 15432 -U $POSTGRES_USER -h $POSTGRES_HOST $POSTGRES_DB -f /output/postgresql_5.3.1_constraint_ddl.sql
-#
-
